9a6ecdb498b1c86ec13b6372491a70ab48f8b27d
andremazevedo <andremazevedo6@gmail.com>
2022-02-05 00:20:25 +0000

    final

--- client/tecnicofs_client_api.c
+++ client/tecnicofs_client_api.c
@@ -13,17 +13,44 @@ int session_id;
 int serverFd, clientFd;
 char client_pipe_name[40];
 
-//DOUBT????????????????????//comment on atomicamente
+// helper function to open a pipe
+int open_pipe(const char *pipename, int flags) {
+    int fd = -1;
+
+    while (fd < 0) {
+        fd = open(pipename, flags);
+        if ((fd == -1) && (errno != EINTR)) {
+            return -1;
+        }
+    }
+
+    return fd;
+}
+
+// helper function to close a pipe
+int close_pipe(int fd) {
+    int ret = -1;
+
+    while (ret < 0) {
+        ret = close(fd);
+        if ((ret == -1) && (errno != EINTR)) {
+            return -1;
+        }
+    }
+
+    return ret;
+}
+
+// helper function to send messages
+// only sends messages atomically, returns -1 otherwise
 ssize_t send_msg(int fd, const void *buf, size_t count) {
     size_t written = 0;
 
     while (written < count) {
         ssize_t ret = write(fd, buf + written, count - written);
         if ((ret == -1) && (errno != EINTR)) {
-            fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));//to remove
             return -1;
         } else if (ret != count) {
-            fprintf(stderr, "[ERR]: write to server failed(not atomic)\n");//to remove 
             return -1;
         }
 
@@ -33,14 +60,14 @@ ssize_t send_msg(int fd, const void *buf, size_t count) {
     return (ssize_t)written;
 }
 
-// What to do on functions if read returns 0(return 0 or -1 eg)???
+// helper function to receive messages
+// returns -1 if server pipe is closed
 ssize_t receive_msg(int fd, void *buf, size_t count) {
     size_t readBytes = 0;
 
     while (readBytes < count) {
         ssize_t ret = read(fd, buf + readBytes, count - readBytes);
         if ((ret == -1) && (errno != EINTR)) {
-            fprintf(stderr, "[ERR]: read failed: %s\n", strerror(errno));//to remove
             return -1;
         } else if (ret == 0) {
             return -1;
@@ -52,33 +79,7 @@ ssize_t receive_msg(int fd, void *buf, size_t count) {
     return (ssize_t)readBytes;
 }
 
-int open_pipe(const char *pipename, int flags) {
-    int fd = -1;
 
-    while (fd < 0) {
-        fd = open(pipename, flags);
-        if ((fd == -1) && (errno != EINTR)) {
-            fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));//to remove
-            return -1;
-        }
-    }
-
-    return fd;
-}
-
-int close_pipe(int fd) {
-    int ret = -1;
-
-    while (ret < 0) {
-        ret = close(fd);
-        if ((ret == -1) && (errno != EINTR)) {
-            fprintf(stderr, "[ERR]: close failed: %s\n", strerror(errno));//to remove
-            return -1;
-        }
-    }
-
-    return ret;
-}
 
 int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
     char tfs_op_code = TFS_OP_CODE_MOUNT;
@@ -87,12 +88,10 @@ int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
     unlink(client_pipe_path);
 
     if (mkfifo (client_pipe_path, 0644) == -1) {
-        perror("Erro no mkfifo");
         return -1;
     }
 
     if ((serverFd = open_pipe(server_pipe_path, O_WRONLY)) == -1) {
-        perror("Erro ao abrir o pipe");
         return -1;
     }
    
@@ -101,17 +100,14 @@ int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
     memcpy(request + 1, client_pipe_path, sizeof(char) * strlen(client_pipe_path));
 
     if (send_msg(serverFd, request, sizeof(request)) == -1) {
-        perror("Erro ao escrever no pipe");
         return -1;
     }
 
     if ((clientFd = open_pipe(client_pipe_path, O_RDONLY)) == -1) {
-        perror("Erro ao abrir o pipe");
         return -1;
     }
 
     if (receive_msg(clientFd, &session_id, sizeof(int)) == -1) {
-        perror("Erro na leitura");
         return -1;
     }
 
@@ -133,22 +129,18 @@ int tfs_unmount() {
     memcpy(request + 1, &session_id, sizeof(int));
 
     if (send_msg(serverFd, request, sizeof(request)) == -1) {
-        perror("Erro ao escrever no pipe");
         return -1;
     }
 
     if (receive_msg(clientFd, &response, sizeof(int)) == -1) {
-        perror("Erro na leitura");
         return -1;
     }
 
     if (close_pipe(clientFd) == -1) {
-        perror("Erro ao fechar o pipe");
         return -1;
     }
 
     if (close_pipe(serverFd) == -1) {
-        perror("Erro ao fechar o pipe");
         return -1;
     }
 
@@ -171,12 +163,10 @@ int tfs_open(char const *name, int flags) {
     memcpy(request + 45, &flags, sizeof(int));
 
     if (send_msg(serverFd, request, sizeof(request)) == -1) {
-        perror("Erro ao escrever no pipe");
         return -1;
     }
 
     if (receive_msg(clientFd, &fd, sizeof(int)) == -1) {
-        perror("Erro na leitura");
         return -1;
     }
 
@@ -193,12 +183,10 @@ int tfs_close(int fhandle) {
     memcpy(request + 5, &fhandle, sizeof(int));
 
     if (send_msg(serverFd, request, sizeof(request)) == -1) {
-        perror("Erro ao escrever no pipe");
         return -1;
     }
 
     if (receive_msg(clientFd, &response, sizeof(int)) == -1) {
-        perror("Erro na leitura");
         return -1;
     }
 
@@ -217,12 +205,10 @@ ssize_t tfs_write(int fhandle, void const *buffer, size_t len) {
     memcpy(request + 17, buffer, sizeof(char) * len);
 
     if (send_msg(serverFd, request, sizeof(request)) == -1) {
-        perror("Erro ao escrever no pipe");
         return -1;
     }
 
     if (receive_msg(clientFd, &r, sizeof(ssize_t)) == -1) {
-        perror("Erro na leitura");
         return -1;
     }
 
@@ -240,12 +226,10 @@ ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
     memcpy(request + 9, &len, sizeof(size_t));
 
     if (send_msg(serverFd, request, sizeof(request)) == -1) {
-        perror("Erro ao escrever no pipe");
         return -1;
     }
 
     if (receive_msg(clientFd, &r, sizeof(ssize_t)) == -1) {
-        perror("Erro na leitura");
         return -1;
     }
 
@@ -254,7 +238,6 @@ ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
     }
 
     if (receive_msg(clientFd, buffer, sizeof(char) * (size_t)r) == -1) {
-        perror("Erro na leitura");
         return -1;
     }
 
@@ -270,12 +253,10 @@ int tfs_shutdown_after_all_closed() {
     memcpy(request + 1, &session_id, sizeof(int));
 
     if (send_msg(serverFd, request, sizeof(request)) == -1) {
-        perror("Erro ao escrever no pipe");
         return -1;
     }
     
     if (receive_msg(clientFd, &response, sizeof(int)) == -1) {
-        perror("Erro na leitura");
         return -1;
     }
 
--- fs/tfs_server.c
+++ fs/tfs_server.c
@@ -19,7 +19,6 @@
  */
 typedef struct {
     int s_id;
-    //char s_buffer[BLOCK_SIZE]; //qual devia ser o tamanho do buffer?
     char *s_buffer;
     pthread_mutex_t s_mutex;
     pthread_cond_t s_cond;
@@ -65,6 +64,8 @@ int server_init() {
     for (int i = 0; i < S; i++) {
         session_table[i].s_id = i;
 
+        session_table[i].s_buffer = NULL;
+
         if (pthread_mutex_init(&session_table[i].s_mutex, NULL) != 0) 
             return -1;
 
@@ -99,14 +100,13 @@ int server_destroy() {
     return 0;
 }
 
-
+// helper function to open a pipe
 int open_pipe(const char *pipename, int flags) {
     int fd = -1;
 
     while (fd < 0) {
         fd = open(pipename, flags);
         if ((fd == -1) && (errno != EINTR)) {
-            fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));//to remove
             return -1;
         }
     }
@@ -114,21 +114,19 @@ int open_pipe(const char *pipename, int flags) {
     return fd;
 }
 
-
+// helper function to close a pipe
 int close_pipe(int fd) {
     int ret = -1;
 
     while (ret < 0) {
         ret = close(fd);
         if ((fd == -1) && (errno != EINTR)) {
-            fprintf(stderr, "[ERR]: close failed: %s\n", strerror(errno));//to remove
             return -1;
         }
     }
 
     return ret;
 }
-
 
 // helper function to send messages
 // retries to send whatever was not sent in the begginning
@@ -139,9 +137,10 @@ ssize_t send_msg(int fd, const void *buf, size_t count, session_t *session) {
         ssize_t ret = write(fd, buf + written, count - written);
         if (ret == -1) {
             if (errno == EPIPE) {
-                session->s_buffer_count = 0;//???
+                session->s_buffer_count = 0;
+                free(session->s_buffer);
+
                 if (close_pipe(session->s_clientFd) == -1) {
-                    perror("Erro ao fechar o pipe");
                     return -1;
                 }
 
@@ -155,21 +154,17 @@ ssize_t send_msg(int fd, const void *buf, size_t count, session_t *session) {
 
                 return 0;
             } else if(errno != EINTR){
-                // if error is diferent from epipe and eintr 
-                fprintf(stderr, "[ERR]: write failed: %s\n", strerror(errno));//to remove
                 return -1;
             }
         }
-
+        
         written += (size_t)ret;
     }
 
     return (ssize_t)written;
 }
 
-
 // helper function to receive messages
-// retries to receive whatever was not received in the begginning//to remove
 ssize_t receive_msg(int fd, void *buf, size_t count) {
     size_t readBytes = 0;
 
@@ -177,7 +172,6 @@ ssize_t receive_msg(int fd, void *buf, size_t count) {
         ssize_t ret = read(fd, buf + readBytes, count - readBytes);
         if (ret == -1) {
             if (errno != EINTR) {
-                fprintf(stderr, "[ERR]: read failed: %s\n", strerror(errno));//to remove
                 return -1;
             }
         } else if (ret == 0) {
@@ -192,41 +186,32 @@ ssize_t receive_msg(int fd, void *buf, size_t count) {
 
 
 
-
-
 void *fnThread(void *arg) {
     session_t *session = (session_t*)arg;
     
     char tfs_op_code;
     for(;;) {
         if (pthread_mutex_lock(&session->s_mutex) != 0) {
-            perror("Erro no lock");// to remove
             exit(-1);
         }
 
-        //printf("\nsession_id on thread: %d\n\n", session->s_id);//to remove
         while (session->s_buffer_count == 0) {
             pthread_cond_wait(&session->s_cond, &session->s_mutex);
         }
 
         memcpy(&tfs_op_code, session->s_buffer,sizeof(char));
-        //printf("tfs_op_code on thread %d: %d.\n", session->s_id, tfs_op_code);//to remove
 
         if (tfs_op_code == TFS_OP_CODE_UNMOUNT) {
-            //printf("TFS_UNMOUNT on thread %d\n", session->s_id);//to remove
             int session_id;
 
             memcpy(&session_id, session->s_buffer + 1, sizeof(int));
-            //printf("session_id on thread %d: %d.\n", session->s_id, session_id);//to remove
 
             if (pthread_mutex_lock(&free_session_mutex) != 0) {
-                perror("Erro no lock");
                 exit(-1);
             }
 
             if (!valid_session(session_id) || free_session[session_id] == AVAILABLE) {
                 if (pthread_mutex_unlock(&free_session_mutex) != 0) {
-                    perror("Erro no lock");
                     exit(-1);
                 }
                 int ret = -1;
@@ -237,46 +222,37 @@ void *fnThread(void *arg) {
             }
 
             if (pthread_mutex_unlock(&free_session_mutex) != 0) {
-                perror("Erro no lock");
                 exit(-1);
             }
 
             int ret = 0;
-            //printf("response on thread %d: %d.\n", session->s_id, ret);//to remove
             if (send_msg(session->s_clientFd, &ret, sizeof(int), session) == -1) {
                 exit(-1);
             }
 
             if (pthread_mutex_lock(&free_session_mutex) != 0) {
-                perror("Erro no lock");
                 exit(-1);
             }
 
             if (close_pipe(session->s_clientFd) == -1) {
-                perror("Erro ao fechar o pipe");
                 exit(-1);
             }
             free_session[session->s_id] = AVAILABLE;
 
             if (pthread_mutex_unlock(&free_session_mutex) != 0) {
-                perror("Erro no lock");
                 exit(-1);
             }
         }
 
         if (tfs_op_code == TFS_OP_CODE_OPEN) {
-            //printf("TFS_OPEN on thread %d\n", session->s_id);//to remove
             char path[40];
             int flags;
 
             memcpy(path, session->s_buffer + 5, sizeof(path));
             memcpy(&flags, session->s_buffer + 45, sizeof(int));
-            //printf("path on thread %d: %s.\n", session->s_id, path);//to remove
-            //printf("flags on thread %d: %d.\n", session->s_id, flags);//to remove
 
             int fd;
             fd = tfs_open(path, flags);
-            //printf("fd on thread %d: %d.\n", session->s_id, fd);//to remove
 
             if (send_msg(session->s_clientFd, &fd, sizeof(int), session) == -1) {
                 exit(-1);
@@ -284,15 +260,12 @@ void *fnThread(void *arg) {
         }
 
         if (tfs_op_code == TFS_OP_CODE_CLOSE) {
-            //printf("TFS_CLOSE on thread %d\n", session->s_id);//to remove
             int fd;
 
             memcpy(&fd, session->s_buffer + 5, sizeof(int));
-            //printf("fd on thread %d: %d.\n", session->s_id, fd);//to remove
 
             int ret;
             ret = tfs_close(fd);
-            //printf("close on thread %d: %d.\n", session->s_id, ret);//to remove
             
             if (send_msg(session->s_clientFd, &ret, sizeof(int), session) == -1) {
                 exit(-1);
@@ -300,22 +273,17 @@ void *fnThread(void *arg) {
         }
 
         if (tfs_op_code == TFS_OP_CODE_WRITE) {
-            printf("TFS_WRITE on thread %d\n", session->s_id);//to remove
             int fhandle;
             size_t len;
 
             memcpy(&fhandle, session->s_buffer + 5, sizeof(int));
             memcpy(&len, session->s_buffer + 9, sizeof(size_t));
-            printf("fhandle on thread %d: %d.\n", session->s_id, fhandle);//to remove
-            printf("len on thread %d: %lu.\n", session->s_id, len);//to remove
 
             char buffer[len];
             memcpy(buffer, session->s_buffer + 17, sizeof(buffer));
-            printf("buffer on thread %d: %s.\n", session->s_id, buffer);//to remove
 
             ssize_t r;
             r = tfs_write(fhandle, buffer, len);
-            printf("r on thread %d: %lu.\n", session->s_id, r);//to remove
 
             if (send_msg(session->s_clientFd, &r, sizeof(ssize_t), session) == -1) {
                 exit(-1);
@@ -323,20 +291,15 @@ void *fnThread(void *arg) {
         }
 
         if (tfs_op_code == TFS_OP_CODE_READ) {
-            //printf("TFS_READ on thread %d\n", session->s_id);//to remove
             int fhandle;
             size_t len;
 
             memcpy(&fhandle, session->s_buffer + 5, sizeof(int));
             memcpy(&len, session->s_buffer + 9, sizeof(size_t));
-            //printf("fhandle on thread %d: %d.\n", session->s_id, fhandle);//to remove
-            //printf("len on thread %d: %lu.\n", session->s_id, len);//to remove
 
             char buffer[len];
             ssize_t r;
             r = tfs_read(fhandle, buffer, len);
-            //printf("r on thread %d: %lu.\n", session->s_id, r);//to remove
-            //printf("buffer on thread %d: %s.\n", session->s_id, buffer);//to remove
 
             if (send_msg(session->s_clientFd, &r, sizeof(ssize_t), session) == -1) {
                 exit(-1);
@@ -349,15 +312,14 @@ void *fnThread(void *arg) {
             }
         }
 
+        free(session->s_buffer);
         session->s_buffer_count--;
         if (pthread_mutex_unlock(&session->s_mutex) != 0) {
-            perror("Erro no unlock");
             exit(-1);
         }
     }
     
 }
-
 
 
 
@@ -374,7 +336,6 @@ int main(int argc, char **argv) {
     char *pipename = argv[1];
     printf("Starting TecnicoFS server with pipe called %s\n", pipename);
 
-    /* TO DO */
     if (tfs_init() == -1) {
         return -1;
     }
@@ -384,21 +345,19 @@ int main(int argc, char **argv) {
 
     for (int i = 0; i < S; i++) {
         if (pthread_create(&tid[i], NULL, fnThread, (void*)&session_table[i]) == -1) {
-            perror("Erro no pthread_create");//to remove return -1;
+            return -1;
         }
     }
 
     unlink(pipename);
     
     if (mkfifo (pipename, 0644) == -1) {
-        perror("Erro no mkfifo");//to remove
         return -1;
     }
 
     signal(SIGPIPE, SIG_IGN);
 
     if ((serverFd = open_pipe(pipename, O_RDONLY)) == -1) {
-        perror("Erro ao abrir o pipe");
         return -1;
     }
 
@@ -413,32 +372,25 @@ int main(int argc, char **argv) {
         }
 
         if (r == 0) {
-            printf("client disconnected\n");//to remove
             if ((serverFd = open_pipe(pipename, O_RDONLY)) == -1) {
-                perror("Erro ao abrir o pipe");
                 return -1;
             }
             continue;
         }
 
-        //printf("tfs_op_code on server: %d.\n", tfs_op_code);//to remove
 
         if (tfs_op_code == TFS_OP_CODE_MOUNT) {
-            //printf("TFS_MOUNT on server\n");//to remove
             int clientFd;
             char client_pipe_path[40];
             if (receive_msg(serverFd, client_pipe_path, sizeof(client_pipe_path)) == -1) {
                 return -1;
             }
-            //printf("client_pipe_path on server: %s.\n", client_pipe_path);//to remove
 
             if ((clientFd = open_pipe(client_pipe_path, O_WRONLY)) == -1) {
-                perror("Erro ao abrir o pipe");
                 return -1;
             }
 
             if (pthread_mutex_lock(&free_session_mutex) != 0) {
-                perror("Erro no lock");
                 return -1;
             }
             
@@ -448,27 +400,15 @@ int main(int argc, char **argv) {
                     free_session[id] = UNAVAILABLE;
                     session_id = id;
 
-                    if (pthread_mutex_lock(&session_table[session_id].s_mutex) != 0) {
-                        perror("Erro no lock");
-                        return -1;
-                    }
-
                     session_table[session_id].s_clientFd = clientFd;
-
-                    if (pthread_mutex_unlock(&session_table[session_id].s_mutex) != 0) {
-                        perror("Erro no unlock");
-                        return -1;
-                    }
                     break;
                 }
             }
 
             if (pthread_mutex_unlock(&free_session_mutex) != 0) {
-                perror("Erro no lock");
                 return -1;
             }
 
-            //printf("session_id on server: %d.\n", session_id);//to remove
             if (send_msg(clientFd, &session_id, sizeof(int), &session_table[session_id]) == -1) {
                 exit(-1);
             }
@@ -485,12 +425,10 @@ int main(int argc, char **argv) {
         }
 
         if (pthread_mutex_lock(&session->s_mutex) != 0) {
-            perror("Erro no lock");
             return -1;
         }
 
         if (tfs_op_code == TFS_OP_CODE_UNMOUNT) {
-            //printf("TFS_UNMOUNT on server\n");//to remove
             char request[5];
             memcpy(request ,&tfs_op_code, sizeof(char));
             memcpy(request + 1 ,&session_id, sizeof(int));
@@ -500,7 +438,6 @@ int main(int argc, char **argv) {
         }
         
         if (tfs_op_code == TFS_OP_CODE_OPEN) {
-            //printf("TFS_OPEN on server\n");//to remove
             char request[49];
             memcpy(request, &tfs_op_code, sizeof(char));
             memcpy(request + 1 ,&session_id, sizeof(int));
@@ -513,7 +450,6 @@ int main(int argc, char **argv) {
         }
 
         if (tfs_op_code == TFS_OP_CODE_CLOSE) {
-            //printf("TFS_CLOSE on server\n");//to remove
             char request[9];
             memcpy(request, &tfs_op_code, sizeof(char));
             memcpy(request + 1 ,&session_id, sizeof(int));
@@ -526,7 +462,6 @@ int main(int argc, char **argv) {
         }
 
         if (tfs_op_code == TFS_OP_CODE_WRITE) {
-            //printf("TFS_WRITE on server\n");//to remove
             int fhandle;
             size_t len;
 
@@ -551,7 +486,6 @@ int main(int argc, char **argv) {
         }
 
         if (tfs_op_code == TFS_OP_CODE_READ) {
-            //printf("TFS_READ on server\n");//to remove
             char request[17];
             memcpy(request, &tfs_op_code, sizeof(char));
             memcpy(request + 1 ,&session_id, sizeof(int));
@@ -564,10 +498,8 @@ int main(int argc, char **argv) {
         }
 
         if (tfs_op_code == TFS_OP_CODE_SHUTDOWN_AFTER_ALL_CLOSED) {
-            //printf("TFS_SHUTDOWN_AFTER_ALL_CLOSED on server\n");//to remove
             int ret;
             ret = tfs_destroy_after_all_closed();
-            //printf("response on server: %d.\n", ret);//to remove
 
             if (send_msg(session->s_clientFd, &ret, sizeof(int), session) == -1) {
                 exit(-1);
@@ -576,17 +508,17 @@ int main(int argc, char **argv) {
             if (ret != -1) {
                 break;
             }
+
+            continue;
         }
         
         session->s_buffer_count++;
         if (pthread_cond_signal(&session->s_cond) != 0) {
             pthread_mutex_unlock(&session->s_mutex);
-            perror("Erro no signal");
             return -1;
         }
 
         if (pthread_mutex_unlock(&session->s_mutex) != 0) {
-            perror("Erro no unlock");
             return -1;
         }
 
@@ -594,18 +526,14 @@ int main(int argc, char **argv) {
 
 
     if (close_pipe(serverFd) == -1) {
-        perror("Erro ao fechar o pipe");
         return -1;
     }
     if (unlink(pipename) != 0) {
-        perror("Erro no unlink");
         return -1;
     }
     if (server_destroy() == -1) {
         return -1;
     }
-
-    printf("Server ended sucessfully!!!!");//to remove
 
     return 0;
 }
